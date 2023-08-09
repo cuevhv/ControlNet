4f0fe77c223a1cd1a1e7d10604db084c57c9fdf7
Hanz <cuevhv@yahoo.com>
2023-08-09 13:59:07 +0200

    added condor seg config

---
+++ condor_configs/condor_seg_cfg/condor_run.sh
@@ -0,0 +1,4 @@
+export PYTHONBUFFERED=1
+export PATH=$PATH
+export TRANSFORMERS_OFFLINE=1
+/home/hcuevas/miniconda3/envs/control2/bin/python train_apple.py --model_cfg_yaml $1 --model_checkpoint $2 --dataset_prompts_json $3 --batch_size 16 --gpus 4 --workers 16 --control_type segmentation
